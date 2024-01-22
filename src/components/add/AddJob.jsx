0a57c29d3fb5d979dc0813af02fc233a01b637ad
jukha <junaid.apsc@gmail.com>
2024-01-23 00:08:45 +0500

    #

--- src/components/add/AddJob.jsx
+++ src/components/add/AddJob.jsx
@@ -64,7 +64,7 @@ function AddJob() {
           <input
             type="text"
             id="title"
-            className="w-full rounded-[34px] border border-gray-200 bg-gray-100 py-3 pl-3 text-sm font-medium placeholder-gray-500 focus:border-gray-400 focus:bg-transparent focus:bg-white focus:outline-none dark:bg-transparent"
+            className="w-full rounded-[34px] border border-gray-200 bg-gray-100 py-3 pl-3 text-sm font-medium placeholder-gray-500 focus:border-gray-400 focus:bg-transparent focus:bg-white focus:outline-none dark:bg-transparent dark:focus:bg-transparent"
             name="title"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
